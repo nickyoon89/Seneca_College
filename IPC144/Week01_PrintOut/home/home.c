1310a95a6bc9c45f046abc6c8e0f5665f6a8dbb7
Jihyun Yoon <nickyoon89@gmail.com>
2018-03-07 21:52:25 -0500

    Create home.c

---
+++ IPC144/Week01_PrintOut/home/home.c
@@ -0,0 +1,12 @@
+******************************
+** Welcome to C Programming **
+******************************
+
+/*
+	Jihyun(Nick)'s at-home-workshop
+*/
+#include <stdio.h>
+int main() {
+	printf("******************************\n** Welcome to C Programming **\n******************************\n"); //display
+	return 0;
+}
