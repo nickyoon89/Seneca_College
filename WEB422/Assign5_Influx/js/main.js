4f66715f8712d63b5c224f2ff40bc9dec6adbc8b
Jihyun Yoon <nickyoon89@gmail.com>
2018-11-15 23:30:28 -0500

    Assignment5 Start Point

---
+++ WEB422/Assign5_Influx/js/main.js
@@ -0,0 +1,45 @@
+jQuery(function($) {'use strict',
+
+	//#main-slider
+	$(function(){
+		$('.carousel').carousel({
+			interval: 4000,
+			pause: "false"
+		});
+	});
+
+
+	// accordian
+	$('.accordion-toggle').on('click', function(){
+		$(this).closest('.panel-group').children().each(function(){
+		$(this).find('>.panel-heading').removeClass('active');
+		 });
+
+	 	$(this).closest('.panel-heading').toggleClass('active');
+	});
+
+
+	// portfolio filter
+	$(window).load(function(){'use strict';
+		var $portfolio_selectors = $('.portfolio-filter >li>a');
+		var $portfolio = $('.portfolio-items');
+		$portfolio.isotope({
+			itemSelector : '.portfolio-item',
+			layoutMode : 'fitRows'
+		});
+		
+		$portfolio_selectors.on('click', function(){
+			$portfolio_selectors.removeClass('active');
+			$(this).addClass('active');
+			var selector = $(this).attr('data-filter');
+			$portfolio.isotope({ filter: selector });
+			return false;
+		});
+	});
+
+
+	//Pretty Photo
+	$("a[rel^='prettyPhoto']").prettyPhoto({
+		social_tools: false
+	});	
+});
