9909aad56de0289e372bb04d5177f0cf8425fc85
Jihyun Yoon <nickyoon89@gmail.com>
2018-12-01 18:03:35 -0500

    assignment6

---
+++ WEB422/Assign6_TempDrivenAngular/app_source/src/app/app.component.ts
@@ -0,0 +1,22 @@
+/*********************************************************************************
+* WEB422 – Assignment 06
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or
+* distributed to other students.
+*
+* Name: Jihyun Yoon Student ID: 124558172 Date: 11/30/2018
+*
+* Heroku Link: https://web422nick.herokuapp.com/
+*
+********************************************************************************/ 
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-dream-app';
+}
